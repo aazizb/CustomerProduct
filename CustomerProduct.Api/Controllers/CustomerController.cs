c97b4b2685478af08b1302dd1ce11ba78b10eaa2
a.a.barry <aazizb@hotmail.com>
2025-03-19 15:54:43 +0100

    Customer endpoits parcialmente implementados

---
+++ CustomerProduct.Api/Controllers/CustomerController.cs
@@ -0,0 +1,22 @@
+﻿using CustomerProduct.Application.Features.Customers;
+
+using MediatR;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace CustomerProduct.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        public CustomerController(IMediator mediator) => Mediator = mediator;
+        [HttpGet("all", Name = "customers")]
+        public async Task<ActionResult<List<CustomerListVm>>> GetAsync()
+        {
+            var result = await Mediator.Send(new GetCustomerListQuery());
+            return Ok(result);
+        }
+        public IMediator Mediator { get; }
+    }
+}
