3cb7108af1fb54a315a82343c954c20ee1d9b339
aazizb <aazizb@hotmail.com>
2025-03-21 05:14:09 +0100

    Refactorización de código

---
+++ CustomerProduct.Api/ApiServicesExtensions.cs
@@ -0,0 +1,44 @@
+﻿using CustomerProduct.Application;
+using CustomerProduct.Infrastructure;
+using CustomerProduct.Persistence;
+
+using Serilog;
+namespace CustomerProduct.Api
+{
+    public static class ApiServicesExtensions
+    {
+        public static WebApplication ConfigureServices(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddApplicationServices();
+            builder.Services.AddPersistenceServices(builder.Configuration);
+            builder.Services.AddInfrastructureServices(builder.Configuration);
+
+            builder.Host.UseSerilog();
+
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            builder.Services.AddControllers();
+
+            var app = builder.Build();
+
+            app.UseSerilogRequestLogging(); // logs HTTP Requests
+
+            return app;
+        }
+        public static WebApplication ConfigurePipeline(this WebApplication app)
+        {
+
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+            app.UseHttpsRedirection();
+            app.MapControllers();
+
+            return app;
+        }
+
+    }
+}
